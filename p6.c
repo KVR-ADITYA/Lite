a09f806deb88674a20c61e9b1d42752378934929
KVR Aditya <kvraditya577@gmail.com>
2019-02-03 14:10:05 +0530

    problem 6

---
+++ p6.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 10
+
+int main(){
+	char str[MAX_STRING_LENGTH];
+	gets(str);
+	int arr[26];
+	memset(arr,0,26*sizeof(int));
+	for(int i=0;i<strlen(str);i++){
+		arr[str[i]-(int)'a']++;
+	}
+	int count = 0;
+	for(int i=0;i<26;i++){
+		if(arr[i]%2 ==1){
+			count++;
+		}
+	}
+	if(count==0 || count%2==1){
+		printf("Yes");
+	}
+	else{
+		for(int i=0;i<26;i++){
+			printf("%d\t",arr[i] );
+		}
+		printf("No\n");
+	}
+}
